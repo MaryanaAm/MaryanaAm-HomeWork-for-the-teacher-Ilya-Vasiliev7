c92387412a085a59b3825bb121ef34e94c9c9163
MaryanaAm <marydeaddoll@gmail.com>
2022-12-25 15:12:42 +0300

    add files

---
+++ 2/Program.cs
@@ -0,0 +1,90 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(-50, 50);
+        }
+    } 
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+
+
+
+void FindNumber(int[,] matrix, int number)
+{
+    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(number == matrix[i, j])
+            {
+                Console.WriteLine($"Число {number} есть в массиве в позиции {i}, {j} ");
+                return;
+            }
+        }        
+    }
+    Console.WriteLine("Такого числа нет");
+    
+}
+
+
+
+
+
+
+int[,] matrix = InitMatrix(5, 5);
+PrintMatrix(matrix);
+
+int numberToFind = GetNumber("Введите число для поиска: ");
+FindNumber(matrix, numberToFind);
